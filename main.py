7a7622d956d93bb50b916e4e916450aa68aae989
Jasurbek Abdukarimov <00012321@wiut.uz>
2021-10-28 11:48:20 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+# import math
+#
+#
+# def area(radius):
+#     a = math.pi*radius**2
+#     return a
+# print(area(2))
+
+# ========= & ===========>
+
+# def absolute_value(x):
+#     if x < 0:
+#        return -x
+#     else:
+#         return x
+#
+#
+# print(absolute_value(4))
+
+# ========= & ===========>
+def compare(x, y):
+    return 1 if x > y else 0 if x == y else 0 if x < y else -1
+print(compare(1, 3))
